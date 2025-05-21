7198f8581d9176e42e024e9b25b77b63b6d9a080
4rGervais <164742695+4rGervais@users.noreply.github.com>
2025-05-21 14:47:06 +0200

    Update solution - CodeHub

--- 4-kyu/trailing-zeros-in-factorials-in-any-given-integer-base/trailing-zeros-in-factorials-in-any-given-integer-base.js
+++ 4-kyu/trailing-zeros-in-factorials-in-any-given-integer-base/trailing-zeros-in-factorials-in-any-given-integer-base.js
@@ -1,5 +1,6 @@
  
 function trailingZeros(num, base) {
+  if(base <2 || base >36 || num <0)return false
   let factorial = 1n, count = 0
   for(let i = 1; i <=num; i++) factorial = Number(factorial) * i
   const changedfactor = factorial.toString(Number(base)).toString().split('').reverse()
