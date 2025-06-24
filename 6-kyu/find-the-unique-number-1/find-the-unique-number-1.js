cc60600041ddbfd36116957e1fe11b72e4af2dd9
4rGervais <164742695+4rGervais@users.noreply.github.com>
2025-06-24 14:58:41 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-unique-number-1/find-the-unique-number-1.js
+++ 6-kyu/find-the-unique-number-1/find-the-unique-number-1.js
@@ -1,5 +1,2 @@
-function findUniq(arr) {
-  const arrCopy = arr.filter(val => arr.indexOf(val) === arr.lastIndexOf(val))
-  return arrCopy.length === 1 ? arrCopy[0] : arr.filter(val => arr.indexOf(val) !== arr.lastIndexOf(val))[0]
-}
+const findUniq = arr => arr.find(val => arr.indexOf(val) === arr.lastIndexOf(val))
 ​
