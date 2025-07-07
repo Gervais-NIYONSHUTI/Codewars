437040183f81002282e94ee279a1ad75e1c6eb12
4rGervais <164742695+4rGervais@users.noreply.github.com>
2025-07-07 12:00:43 +0200

    Add solution - CodeHub

---
+++ 6-kyu/walk-up-stairs/walk-up-stairs.js
@@ -0,0 +1,9 @@
+function stairs(n){
+    let arr = []
+    for(let i = 1; i <= n; i++) {
+        let a = []
+        for(let x = 1; x <=i; x++) a.push(+`${x}`.slice(-1))
+        arr.push( [...a,...a.reverse()].join(' ')) 
+    }
+    return arr.map(val => (' '.repeat(arr[arr.length - 1].length - val.length) + val)).join('\n')
+}
