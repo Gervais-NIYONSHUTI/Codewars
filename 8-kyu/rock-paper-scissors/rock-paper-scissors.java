55d7dbaeab3d6a381e4ef4cef4c3abadee2147f6
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2025-10-20 10:49:07 +0200

    Update solution - CodeHub

--- 8-kyu/rock-paper-scissors/rock-paper-scissors.java
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.java
@@ -1,23 +1,24 @@
+ 
 public class Kata {
   public static String rps(String p1, String p2) {
     if(p1.equals(p2)) return "Dram";
     if(p1.equals("scissors")){
       if(p2.equals("rock")){
-        return "Player 2 won";
+        return "Player 2 won!";
       } else {
-        return "Player 1 won";
+        return "Player 1 won! ";
       }
     } else if (p1.equals("paper")){
       if(p2.equals("rock")){
-        return "Player 2 won";
+        return "Player 2 won!";
       } else {
-        return "Player 1 won";
+        return "Player 1 won!";
       }
     } else {
       if(p2.equals("paper")){
-        return "Player 2 won";
+        return "Player 2 won!";
       } else {
-        return "Player 1 won";
+        return "Player 1 won!";
       }
     }
   }
