a43acf3bdc973736d708d07321302f05226f69e8
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2025-10-20 16:45:16 +0200

    Update solution - CodeHub

--- 7-kyu/highest-and-lowest/highest-and-lowest.java
+++ 7-kyu/highest-and-lowest/highest-and-lowest.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Kata {
   public static String highAndLow(String numbers) {
-    String[] numbersArray = Arrays.stream(numbers.split(" ")).map(num -> Integer.parseInt(num));
+    int[] numbersArray = Arrays.stream(numbers.split(" ")).map(num -> Integer.parseInt(num));
     System.out.println(numbersArray);
     return "throw towel";
   }
