8b9c3faa50f8a046a957f3865b14b8b53a809df4
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2025-10-20 10:34:03 +0200

    Update solution - CodeHub

--- 8-kyu/the-if-function/the-if-function.java
+++ 8-kyu/the-if-function/the-if-function.java
@@ -1,7 +1,8 @@
+ 
 public class Kata {
   public static void _if(Boolean bool, Runnable func1, Runnable func2) {
     if(bool){
-      Runnable func1();
+      Runnable func1;
     } else {
       func2();
     }
