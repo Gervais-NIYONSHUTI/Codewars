34bff9dc71c41a3f4233a303a72dd5739c81d563
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2025-10-20 14:59:03 +0200

    Add solution - CodeHub

---
+++ 7-kyu/highest-and-lowest/highest-and-lowest.java
@@ -0,0 +1,8 @@
+ 
+public class Kata {
+  public static String highAndLow(String numbers) {
+    String[] numbersArray = numbers.split(" ");
+    System.out.println(numbersArray);
+    return "throw towel";
+  }
+}
