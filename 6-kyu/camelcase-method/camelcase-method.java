f5cb1caa91666bb22fab755bd045a579eae17cb3
Gervais NIYONSHUTI <164742695+Gervais-NIYONSHUTI@users.noreply.github.com>
2025-11-03 09:57:50 +0200

    Add solution - CodeHub

---
+++ 6-kyu/camelcase-method/camelcase-method.java
@@ -0,0 +1,16 @@
+ 
+import java.util.Arrays;
+import java.util.List;
+​
+public class Solution {
+​
+    public static String camelCase(String str) {
+        System.out.println(str);
+        String[] words = str.split(" ");
+        words = Arrays.stream(words).filter(val -> val != "").toArray(String[]::new);
+        List wordsArr = Arrays.stream(words).map(val -> String.valueOf(val.charAt(0)).toUpperCase() + val.substring(1)).toList();
+        return String.join( "" ,wordsArr);
+    }
+​
+}
+​
